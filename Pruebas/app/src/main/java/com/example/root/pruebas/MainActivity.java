3b9d972d266d938645c47ed7c012dbfcb60f80e3
jafidiesel <jafibravin@outlook.com>
2016-12-05 17:16:40 -0300

    Aplicado theme en Pruebas

--- Pruebas/app/src/main/java/com/example/root/pruebas/MainActivity.java
+++ Pruebas/app/src/main/java/com/example/root/pruebas/MainActivity.java
@@ -7,10 +7,12 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
 
     public void acercade(View view) {
